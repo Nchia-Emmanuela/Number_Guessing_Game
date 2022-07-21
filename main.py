3fdfff6ef248e28f8c5315e96561b662381f31dd
Nchia Emmanuela <ekeinchaiemmanuela@gmail.com>
2022-07-21 12:33:52 +0100

    check answer function defined

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import os
 
 def clear():
     os.system('cls')
+
+def check_answer(guess, answer):
+    """Takes in guess and the answer and compares them"""
+    if guess > answer:
+        print("Too high")
+    elif guess < answer:
+        print("Too low")
+    else:
+        print(f"You gote it! the answer is {answer}")
