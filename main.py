ecb76492b611b0ab63c168e714511f2811337da8
Nchia Emmanuela <ekeinchaiemmanuela@gmail.com>
2022-07-21 13:47:02 +0100

    functionality added for user to play again

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ HARD_LEVEL = 5
 def clear():
     os.system('cls')
 
-def check_answer(guess, answer):
-    """Takes in guess and the answer and compares them"""
+def check_answer(guess, answer, turns):
+    """Takes in guess and the answer, compares them and returns the number of turns remaining """
     if guess > answer:
         print("Too high")
+        return turns -1
     elif guess < answer:
         print("Too low")
+        return turns -1
     else:
         print(f"You gote it! the answer is {answer}")
 
@@ -28,17 +30,23 @@ def set_difficulty():
 
 
 def game():
+    """Main functionality of game"""
+    print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a Number between 1 and 100.")
     answer = randint(1, 100)
 
     turns = set_difficulty()
-    print(f"You have {turns} attempts, to guess the number")
     
-
     guess = 0
     while guess != answer:
+        print(f"You have {turns} attempts, to guess the number")
         guess = int(input("Make a guess: "))
-        check_answer(guess, answer)
-        turns -= 1
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You ran out of lives, You lose!")
+            return
 game()
+while input("\nDo you wish to play again, yes or no?: ") == 'yes':
+    clear()
+    game()
