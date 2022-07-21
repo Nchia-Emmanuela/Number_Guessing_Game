4ad69af10454ce7cee9ac4d7bca7cdc4c967e937
Nchia Emmanuela <ekeinchaiemmanuela@gmail.com>
2022-07-21 12:40:39 +0100

    game difficulty level

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from random import randint
 from arts import logo
 import os
 
+run_time = 0
+
 def clear():
     os.system('cls')
 
@@ -13,3 +15,12 @@ def check_answer(guess, answer):
         print("Too low")
     else:
         print(f"You gote it! the answer is {answer}")
+
+def set_difficulty():
+    """Sets the difficulty level of the Game either to hard or easy"""
+    level = input("Choose the level of difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        run_time = 10
+    elif level == 'hard':
+        run_time = 5
+    return run_time
