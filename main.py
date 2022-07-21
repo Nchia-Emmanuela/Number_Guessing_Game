cd2e77fd484a41b5b4401f9e21457d06f9d319ad
Nchia Emmanuela <ekeinchaiemmanuela@gmail.com>
2022-07-21 13:08:20 +0100

    refactoring

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import os
 
 run_time = 0
 
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a Number between 1 and 100.")
+answer = randint(1, 100)
+
 def clear():
     os.system('cls')
 
@@ -16,11 +22,20 @@ def check_answer(guess, answer):
     else:
         print(f"You gote it! the answer is {answer}")
 
+level = input("Choose the level of difficulty. Type 'easy' or 'hard': ")
 def set_difficulty():
     """Sets the difficulty level of the Game either to hard or easy"""
-    level = input("Choose the level of difficulty. Type 'easy' or 'hard': ")
     if level == 'easy':
-        run_time = 10
+        return EASY_LEVEL
     elif level == 'hard':
-        run_time = 5
-    return run_time
+        return HARD_LEVEL
+
+
+
+turns = set_difficulty()
+print(f"You have {turns} attempts, to guess the number")
+guess = int(input("Make a guess: "))
+
+while turns > 0:
+    check_answer(guess, answer)
+    turns -= 1
